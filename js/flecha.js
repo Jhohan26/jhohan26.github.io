289d52a1285a7316e63126ac78e5a09a300895dc
Jhohan <jhohan.reyes@uniminuto.edu.co>
2024-01-09 21:53:25 -0500

    Se ha arreglado un bug menor

--- js/flecha.js
+++ js/flecha.js
@@ -1,10 +1,9 @@
 var flecha = document.getElementById("flecha");
 
 function scroll(){
-	let ancho_menu = document.getElementsByTagName("header")[0].offsetHeight;
 	let ancho_inicio = document.getElementById("inicio").offsetHeight;
 
-	window.scrollTo(0,ancho_inicio + ancho_menu);
+	window.scrollTo(0, ancho_inicio);
 }
 
 flecha.addEventListener("mouseenter", scroll);
